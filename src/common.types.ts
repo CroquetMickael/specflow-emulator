87c13d63def5ce66d83ec27a43303fbb0b043b2c
Croquet Mickael <mickael.croquet@laposte.net>
2023-02-11 12:05:05 +0100

    fix(Steps): Modifying types not allowing feature and tag in the same object, that would make crash the emulator

--- src/common.types.ts
+++ src/common.types.ts
@@ -1,26 +1,38 @@
 import {
-    DefineStepFunction,
-    StepsDefinitionCallbackOptions,
-} from 'jest-cucumber/dist/src/feature-definition-creation';
+  DefineStepFunction,
+  StepsDefinitionCallbackOptions,
+} from "jest-cucumber/dist/src/feature-definition-creation";
 
 export interface IScope {
-    feature: string;
-    scenario?: string;
-    tag?: string;
+  feature?: string;
+  scenario?: string;
+  tag?: string;
 }
+
+export type ScopeFeatureType = {
+  feature?: string;
+  scenario?: string;
+  tag?: never;
+};
+
+export type ScopeTagType = {
+  feature?: never;
+  scenario?: string;
+  tag?: string;
+};
 
 export type StepMatcher = string | RegExp;
 
-export type StepBlock = 'given' | 'when' | 'then';
+export type StepBlock = "given" | "when" | "then";
 
 export type JestCallbackSelector = (
-    jestStepCallbacks: StepsDefinitionCallbackOptions,
+  jestStepCallbacks: StepsDefinitionCallbackOptions
 ) => DefineStepFunction;
 
 export type StepCallback = (scenarioContext: any) => (...args: any[]) => any;
 
 export type DefineStepCallback = (
-    match: StepMatcher,
-    callback: StepCallback,
-    scopes?: IScope[],
+  match: StepMatcher,
+  callback: StepCallback,
+  scopes?: ScopeFeatureType[] | ScopeTagType[]
 ) => void;
